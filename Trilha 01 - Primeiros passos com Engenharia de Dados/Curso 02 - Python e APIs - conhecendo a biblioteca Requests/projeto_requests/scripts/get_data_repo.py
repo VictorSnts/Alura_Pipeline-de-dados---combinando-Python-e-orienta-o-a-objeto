bf162076d5b814573cdad6b58c3769ac6f0f81ce
Victor Santos <vfreitass16@icloud.com>
2024-02-10 21:25:52 -0300

    Criando script para executar o ETL

---
+++ Trilha 01 - Primeiros passos com Engenharia de Dados/Curso 02 - Python e APIs - conhecendo a biblioteca Requests/projeto_requests/scripts/get_data_repo.py
@@ -0,0 +1,180 @@
+
+import requests
+import pandas as pd
+import base64
+from datetime import datetime
+
+DATE = datetime.now().strftime('%Y%m%d')
+ACCESS_TOKEN = "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"
+HEADERS = {'Authorization' : 'Bearer ' + ACCESS_TOKEN,
+           'X-GitHub-Api-Version': '2022-11-28'}
+BASE_URL_API = "https://api.github.com"
+USER_COMPANY = "amzn"
+SAVE_DATA_INFO = {
+    "file_name" : f"{USER_COMPANY}_repos_languages_{DATE}.csv",
+    "owner" : "VictorSnts",
+    "repo" : "linguagens-utilizadas-empresas"
+}
+
+def print_info(info):
+    print(f"INFO: {info}")
+    print("--")
+
+
+def get_pages_num(url):
+    response = requests.get(url)
+    link_header = response.headers.get('link')
+
+    if link_header:
+        links = link_header.split(',')
+        last_page_info = next(link for link in links if 'rel="last"' in link)
+        total_pages = int(last_page_info.split(';')[0].split("page=")[-1].replace(">", ""))
+        return total_pages
+    else:
+        raise Exception(ReferenceError, f"Nao foi possivel identificar o numero de paginas. Código de resposta: {response.status_code}")
+
+
+def get_repos(url):
+    repos_list = []
+    sum_repos = 0
+    pages_num = get_pages_num(url)
+    print_info(f"Numero de paginas que serao acessadas: {pages_num}")
+
+    page = 1
+    while True:
+        try:
+            url_page = f"{url}?page={page}"
+            print_info(f"URL da pagina: {url_page}")
+            response = requests.get(url_page, headers=HEADERS)
+            repos_list.append(response.json())
+            num_repos = len(response.json())
+            print_info(f"Numero de repos na pagina {page}: {num_repos}")
+            sum_repos += num_repos
+        except:
+            repos_list.append(None)
+        if page == pages_num: break
+        page += 1
+
+    print_info(f"Total de repositorios: {sum_repos}")
+    return(repos_list)
+
+
+def get_repos_name(repos):
+    list_repos_name = []
+
+    for page in repos_list:
+        for repo in page:
+            list_repos_name.append(repo["name"])
+
+    return list_repos_name
+
+
+def get_repos_language(repos):
+    list_repos_language = []
+
+    for page in repos_list:
+        for repo in page:
+            list_repos_language.append(repo["language"])
+
+    return list_repos_language
+
+
+def create_df(data):
+    return pd.DataFrame(data)
+
+
+def save_data(data, file_name):
+    df = create_df(data)
+    df.to_csv(file_name, index=False)
+
+
+def repo_exists(owner, repo):
+    url = f'{BASE_URL_API}/repos/{owner}/{repo}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        return True
+    elif response.status_code == 404:
+        return False
+    else:
+        raise Exception(ReferenceError, f"Erro ao verificar o repositório {repo}. Código de resposta: {response.status_code}")
+
+
+def create_repo(repo, description):
+    url = f"{BASE_URL_API}/user/repos"
+    data = {
+        "name" : repo,
+        "description" : description,
+        "private" : False
+    }
+
+    response = requests.post(url, json=data, headers=HEADERS)
+    if response.status_code == 201:
+        print_info(f"Repositorio {repo} criado com sucesso.")
+    else:
+        raise Exception(ReferenceError, f"Erro ao criar o repositório {repo}. Código de resposta: {response.status_code}")
+
+
+def file_to_base64(file_name):
+    with open(file_name, "rb") as file:
+        file_content = file.read()
+
+    return base64.b64encode(file_content)
+
+
+def load_data(file_name, owner, repo):
+    url = f"{BASE_URL_API}/repos/{owner}/{repo}/contents/{file_name}"
+    encoded_file = file_to_base64(file_name)
+
+    data = {
+        "message" : "Importando novo aquivo de dados",
+        "content" : encoded_file.decode("utf-8")
+        }
+
+    response = requests.put(url, json=data, headers=HEADERS)
+    if response.status_code == 201:
+        print_info(f"Arquivo {file_name} carregado com sucesso.")
+    else:
+        raise Exception(ReferenceError, f"Erro ao criar o repositório {repo}. Código de resposta: {response.status_code}")
+
+
+# Exportando os Dados
+print_info(f"EXPORTANDO OS DADOS")
+print_info(f"Obtendo infos completas dos repositorios de {USER_COMPANY}")
+url_export = f"{BASE_URL_API}/users/{USER_COMPANY}/repos"
+repos_list = get_repos(url_export)
+
+
+# Transformando os Dados
+print_info(f"TRANSFORMANDO OS DADOS")
+print_info(f"Obtendo nomes dos repositorio de {USER_COMPANY}")
+list_repos_name = get_repos_name(repos_list)
+
+print_info(f"Obtendo linguagens de programação dos repositorios de {USER_COMPANY}")
+list_repos_language = get_repos_language(repos_list)
+
+data = {
+    "repository" : list_repos_name,
+    "language" : list_repos_language
+}
+
+print_info(f"Salvando dados dos repositorios de {USER_COMPANY} no arquivo {SAVE_DATA_INFO['file_name']}")
+save_data(data, SAVE_DATA_INFO["file_name"])
+
+
+# Carregando os dados
+print_info(f"CARREGANDO OS DADOS")
+url_load = f"{BASE_URL_API}/user/repos"
+
+print_info(f"Verificando o repositorio {SAVE_DATA_INFO['repo']}, para carregar o arquivo.")
+if repo_exists(SAVE_DATA_INFO["owner"], SAVE_DATA_INFO["repo"]):
+    print_info(f"O repositorio {SAVE_DATA_INFO['repo']} ja existe.")
+else:
+    print_info(f"O repositorio {SAVE_DATA_INFO['repo']} nao existe. Criando o repositorio.")
+    create_repo(SAVE_DATA_INFO['repo'], "Criando repo para carga de arquivos referente as linguagens de programa'xão utilizadas nas empresas")
+
+print_info("Carregando o arquivo.")
+load_data(SAVE_DATA_INFO["file_name"], SAVE_DATA_INFO["owner"], SAVE_DATA_INFO['repo'])
+
+
+
